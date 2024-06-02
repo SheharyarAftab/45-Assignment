03e22df084ad7e6c0143a58a06700de08d8672db
sheharyarAftab <sheharyar9292162@gmail.com>
2024-06-02 20:18:27 +0500

    first commit

---
+++ Question No 2/main.js
@@ -0,0 +1,2 @@
+var personName = 'Sherii';
+console.log('Hello ${personName}, would you like to learn some typescript today?');
---
+++ Question No 2/main.ts
@@ -0,0 +1,3 @@
+let personName :string = 'Sherii';
+
+console.log ('Hello ${personName}, would you like to learn some typescript today?')
